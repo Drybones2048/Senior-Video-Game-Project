481f751b7d4fc95501835492892b892b9262981d
NathanGahan <100215985+NathanGahan@users.noreply.github.com>
2026-03-01 21:13:50 -0500

    Added CardLibrary file
    Added CardLibrary scriptable object script. This is what manages the total library of cards and other scripts must use it to form their decks for that run.
    
    It has a list "cardList" which is a complete list of all unique CardData(cards). However other scripts can't and shouldn't access this directly. Instead it can call the GetCardByID function to get a specific card by referencing the ID.
    
    Also there's a GetRandomCard method which will be used for random card drops after fights. You can call this and specify how many cards you want and other parameters like which character class the cards belong to and it will return that many unique cards. Of course if you try and ask for more cards than there are with that set of parameters then you won't get as many cards you asked for(or any).

--- 2D Roguelike Deckbuilder/Assets/Scripts/ScriptableObjects/CardData.cs
+++ 2D Roguelike Deckbuilder/Assets/Scripts/ScriptableObjects/CardData.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 
 public enum CardType { Attack, Defend, Special, Persistent }
 public enum UniqueBehavior { None, PressAndFall, PiercingStrike } //going to add more here once I decide what needs unique behavior
+public enum CardClass { All, BlockStance, StatusEffectStance, AttackStance }
+public enum IsStartingCard { Yes, No }    //Is this card in the class's deck at the start of each run?
 
 [CreateAssetMenu(fileName = "CardData", menuName = "Scriptable Objects/CardData")]
 public class CardData : ScriptableObject
 {
     [Header("Card Attributes")]
-    public string name;
+    public string id;
+    public string displayName;
     public CardType type;
     public int cost;
 
@@ -17,4 +20,6 @@ public class CardData : ScriptableObject
 
     public List<StatusEffect> statusEffects;
     public UniqueBehavior uniqueBehavior;   //hardcoding behavior for certain cards, logic resolved in another script
+    public CardClass cardClass;
+    public IsStartingCard isStartingCard;
 }
---
+++ 2D Roguelike Deckbuilder/Assets/Scripts/ScriptableObjects/CardLibrary.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "CardLibrary", menuName = "Scriptable Objects/CardLibrary")]
+public class CardLibrary : ScriptableObject
+{
+    [SerializeField] private List<CardData> cardList = new();
+    Dictionary<string, CardData> cardDict = new Dictionary<string, CardData>();
+
+    void OnEnable() {
+        cardDict.Clear();   //in case CardLibrary were somehow disabled and re-enabled during runtime
+
+        foreach (CardData card in cardList) {
+            //safeguard in case two cards are accidentally created with the same ID
+            if (cardDict.ContainsKey(card.id))
+            {
+                Debug.LogError($"Duplicate card id detected: {card.id}", this);
+                continue;
+            }
+
+            cardDict.Add(card.id, card);
+        }
+    }
+
+    public CardData GetCardById(string id) {
+        if (cardDict.ContainsKey(id))
+        {
+            return cardDict[id];
+        }
+        else {
+            throw new KeyNotFoundException($"Card name '{id}' was not found in CardLibrary.");
+        }
+    }
+
+    public List<string> GetRandomCard(System.Predicate<CardData> filter, int numCardsRequested)
+    {
+        var newCardPool = new List<CardData>();
+
+        foreach (var c in cardList)
+            if (c != null && filter(c))
+                newCardPool.Add(c);
+
+        if (newCardPool.Count == 0)
+        {
+            Debug.Log("Card requested but no cards match the criteria.");
+            return new List<string>();
+        }
+
+        // Shuffle
+        for (int i = 0; i < newCardPool.Count; i++)
+        {
+            int j = RoundManager.instance.RNG.Next(i, newCardPool.Count);
+            (newCardPool[i], newCardPool[j]) = (newCardPool[j], newCardPool[i]);
+        }
+
+        int numCardsReturning = Mathf.Min(numCardsRequested, newCardPool.Count);    //this would only not equal numCardsRequested if more cards were requested than met the criteria
+
+        var selectedCardIDs = new List<string>();
+
+        //now that the deck is shuffled, you can just select the number of cards you need starting from index 0. 
+        for (int i = 0; i < numCardsReturning; i++)
+            selectedCardIDs.Add(newCardPool[i].id);
+
+        return selectedCardIDs;
+    }
+}
